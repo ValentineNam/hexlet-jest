026e8e2e918141f9c880888fd24786f48833b7e9
v.nam <v4lentine.nam@gmail.com>
2024-02-28 10:26:03 +0300

    Add comments to code

--- src/index.js
+++ src/index.js
@@ -9,47 +9,59 @@ export default class ShoppingCart {
     this.discountCode = null;
   }
 
+// Метод добавления товара в корзину, по умолчанию количество, идущее 2м аргументом = 1
   addItem(item, quantity = 1) {
     const existingItem = this.items.find(i => i.item === item);
+// Это проверка на то, находится ли уже товар в корзине и если да, то увеличиваем его кол-во на quntity
     if (existingItem) {
         existingItem.quantity += quantity;
     } else {
+// Иначе добавляем в массив элементов корзины
         this.items.push({ item, quantity });
     }
+// Пересчитываем сумму
     this.calculateTotal();
   }
 
+// Метод удаления товара из корзины
   removeItem(name) {
     this.items = this.items.filter((elem) => elem.item.name !== name);
+// Пересчитываем сумму
     this.calculateTotal();
   }
 
+// Обновление кличества товара с определенным именем, исходя из quantity
   updateQuantity(name, quantity) {
     const foundItem = this.items.find(({ item }) => item.name === name);
 
     if (foundItem) {
       foundItem.quantity = quantity;
+// Пересчитываем сумму
       this.calculateTotal();
     }
   }
 
+// Расчет общей суммы, исходя из позиций в корзине (вызывается в большинстве методов)
   calculateTotal() {
     this.total = this.items.reduce((acc, elem) => acc + elem.item.price * elem.quantity, 0);
   }
 
+// Метод очистки корзины (введенный скидочны код при этом не сбрасывается)
   clearCart() {
     this.items = [];
     this.total = 0;
   }
 
+// Метод добавления скидки по коду, проверяет валиден ли он на текущий день
   applyDiscount(codeName) {
     const codeObj = codes.find((code) => code.name === codeName);
     const currentDate = new Date();
-
+// Если код валидный (существует или не просрочен), то пересчитываем цену
     if (codeObj && new Date(codeObj.expireDate) > currentDate) {
       this.total *= (100 - codeObj.percentage) / 100;
       this.discounted = true;
       this.discountCode = codeName;
+// Иначе сбрасываем код (в тч, если уже какой-то указан)
     } else {
       this.discounted = false;
       this.discountCode = null;
@@ -68,6 +80,8 @@ myCart.addItem(item3, 3);
 myCart.updateQuantity(item1.name, 4);
 console.log(myCart);
 myCart.applyDiscount('validCode');
+console.log(myCart);
+myCart.applyDiscount('invalidCode');
 console.log(myCart);
 myCart.clearCart();
 console.log(myCart);
